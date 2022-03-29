6bca67939af33bb9bf48b8da36ca8faa2b55dee0
Anthony Castaneda <73725048+antdancast@users.noreply.github.com>
2022-03-29 13:01:25 -0400

    Add files via upload

---
+++ Postix 03292022/proc_stusign.php
@@ -0,0 +1,45 @@
+<?php
+include ('con.php');
+
+$error = null;
+
+
+if (isset($_POST['sign2']))
+{
+    
+    $name = mysqli_real_escape_string($con,$_POST['name']);
+    $email = mysqli_real_escape_string($con,$_POST['email']);
+    $PASSWORD = mysqli_real_escape_string($con,$_POST['psw']);
+    $COPY = mysqli_real_escape_string($con,$_POST['psw-repeat']);
+    if (strlen($PASSWORD) >= 8){
+
+        if($PASSWORD==$COPY){
+
+            $sql = "INSERT INTO students (semail,suser,spass) VALUES ('$email','$name','$PASSWORD')";
+            
+            if (mysqli_query($con, $sql)){
+                
+                header("Location: StudentLogin.php");
+            }
+            else
+            {
+                echo "Error: " . $sql . "<br>" . mysqli_error($con);
+                $error = "Error: Please return to sign up page";
+            }
+        }
+        else{
+            $error = "Passwords are not matching!";
+        }
+
+    }
+    else{
+        $error = "Passsword length has to be at least 8 characters!";
+    }
+
+}
+
+$con->close();
+
+?>
+
+
