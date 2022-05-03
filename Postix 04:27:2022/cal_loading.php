fe3c8fa9cf87f101c09b4cbbca1afbe509001737
lovatop1 <69662630+lovatop1@users.noreply.github.com>
2022-05-03 12:42:14 -0400

    headers

--- Postix 04:27:2022/cal_loading.php
+++ Postix 04:27:2022/cal_loading.php
@@ -1,6 +1,10 @@
 <?php
 include ('con.php');
 session_start();
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: POST, GET, PUT, OPTIONS, PATCH, DELETE');
+header('Access-Control-Allow-Credentials: true');
+header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 
 $data = array();
 
